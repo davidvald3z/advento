6078ed18373a19552a5329d53a8a3e13f3b30629
David Valdez <david_valdez@hotmail.com>
2024-11-21 15:22:37 -0600

    halfway digits

--- app/models/advento.rb
+++ app/models/advento.rb
@@ -16,5 +16,25 @@ class Advento
     
         result
     end
+
+    def halfway_digits_match(input)
+        result = 0
+        input_arr = input.split("")
+        halfway = input_arr.size/2
+        
+        input_arr.each_with_index do |digit, index|
+            if index == (input_arr.size - 1)
+                if digit == input_arr[halfway]
+                    result = result + digit.to_i
+                end
+            else
+                if digit == input_arr[halfway + index + 1]
+                    result = result + digit.to_i
+                end
+            end
+        end
+
+        result
+    end
 end
 
